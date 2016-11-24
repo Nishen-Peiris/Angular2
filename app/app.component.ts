fdf2abacb5e18679634b2550dfac02f675addd00
Nishen Peiris <Nishen Peiris>
2016-11-24 11:44:13 +0530

    Finish multipleDatesPicker

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,31 +4,19 @@ declare var jQuery:any;
 
 @Component({
   selector: 'my-app',
-  template: `<input type="text" class="amount" required placeholder="Enter a name here">
-			<div class="slider"></div>
-			<h2>slideValue = {{slideValue}}</h2>`,
+  template: `Enter Date: <div id="multiDatesPicker"></div>`,
 })
 export class AppComponent  {
-	name = 'Angular';
 	elementRef: ElementRef;
-    slideValue: number;
 
     constructor(@Inject(ElementRef) elementRef: ElementRef) {
         this.elementRef = elementRef;
     }
 
     ngOnInit() {
-        var $amount = jQuery(this.elementRef.nativeElement).find(".amount");
-        jQuery(this.elementRef.nativeElement).find(".slider").slider({
-            range: false,
-            orientation: "vertical",
-            min: 0,
-            max: 100,
-            value: 60,
-            slide: (event:any, ui:any) => {
-                this.slideValue = ui.value;
-                $amount.val(ui.value);
-            }
-        });
+        jQuery(this.elementRef.nativeElement).find("#multiDatesPicker").multiDatesPicker({
+			numberOfMonths: [3,4],
+			defaultDate:"1/1/2016"
+		});
     }
 }

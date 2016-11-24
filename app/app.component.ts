4c1d5906b7ae500a257e906bd27d28d7494b6171
Nishen Peiris <Nishen Peiris>
2016-11-24 11:22:49 +0530

    Finish Slider

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit } from '@angular/core';
+
+declare var jQuery:any;
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<input type="text" class="amount" required placeholder="Enter a name here">
+			<div class="slider"></div>
+			<h2>slideValue = {{slideValue}}</h2>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+	name = 'Angular';
+	elementRef: ElementRef;
+    slideValue: number;
+
+    constructor(@Inject(ElementRef) elementRef: ElementRef) {
+        this.elementRef = elementRef;
+    }
+
+    ngOnInit() {
+        var $amount = jQuery(this.elementRef.nativeElement).find(".amount");
+        jQuery(this.elementRef.nativeElement).find(".slider").slider({
+            range: false,
+            orientation: "vertical",
+            min: 0,
+            max: 100,
+            value: 60,
+            slide: (event:any, ui:any) => {
+                this.slideValue = ui.value;
+                $amount.val(ui.value);
+            }
+        });
+    }
+}
